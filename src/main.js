3891c5f4bd99af2e9f4e5b3d696b876a06e395f0
Fabian S <1910653941@stud.fh-kufstein.ac.at>
2021-11-09 00:34:32 +0000

    adjusted firebase connection, created createEvent, createUser, getEvents, getEvent methods

--- src/main.js
+++ src/main.js
@@ -2,23 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore/lite'
 
 Vue.config.productionTip = false
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyCFpZuP7zxZeTqHqQvfhbDSfvJa_U-Kyr0',
-  authDomain: 'fahmos.firebaseapp.com',
-  projectId: 'fahmos',
-  storageBucket: 'fahmos.appspot.com',
-  messagingSenderId: '719976341701',
-  appId: '1:719976341701:web:33eb6ad4ca1838d98850ac',
-  measurementId: 'G-VSF1KXQCK5'
-}
-
-const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
 
 new Vue({
   router,
