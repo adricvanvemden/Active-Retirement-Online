abdaa176ad3a9b8855424a6ac05819ffb66f315c
Manuel Dolenz <1910653532@stud.fh-kufstein.ac.at>
2021-11-16 22:40:26 +0000

    eventView 1.0

---
+++ src/components/Events.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="games">
+    <div class="header">
+      <h1>Hey "username", click on one of these buttons to view all the specific events.</h1>
+    </div>
+    <div class='some-page-wrapper'>
+      <div class='row'>
+        <div class='column'>
+          <div class='column-one' onclick="location.href='/events/food_events'">
+            FOOD
+          </div>
+          <div class='column-one' onclick="location.href='/events/trip_events'">
+            TRIPS
+          </div>
+        </div>
+        <div class='column'>
+          <div class='column-two' onclick="location.href='/events/game_events'">
+            GAMES
+          </div>
+          <div class='column-two' onclick="location.href='/events/webinar_events'">
+            WEBINAR
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  .some-page-wrapper {
+    margin-top: 50px;
+  }
+  .row {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+    cursor: pointer;
+  }
+  .column {
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 1;
+  }
+  .column-one {
+    height: 120px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border: 3px solid #d3d3d3;
+    border-radius: 18px;
+    margin: 50px 50px 50px 200px;
+    font-size: 40px;
+    padding-right: 100px;
+    background: #f3f4f5;
+  }
+  .column-two {
+    height: 120px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border: 3px solid #d3d3d3;
+    border-radius: 18px;
+    margin: 50px 200px 50px 50px;
+    padding-right: 100px;
+    font-size: 40px;
+    background: #f3f4f5;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -43,12 +43,45 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "signIn" */ '../views/SignIn.vue')
   },
-    path: '/admin/events/create',
-    name: 'createEvent',
+  {
+    path: '/myAccount',
+    name: 'MyAccount',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "events-admin" */ '../views/CreateEvent.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/MyAccount.vue')
+  },
+  {
+    path: '/events/food_events',
+    name: 'FoodEvents',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "events" */ '../views/FoodEvents.vue')
+  },
+  {
+    path: '/events/game_events',
+    name: 'GameEvents',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "events" */ '../views/GameEvents.vue')
+  },
+  {
+    path: '/events/trip_events',
+    name: 'TripEvents',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "events" */ '../views/TripEvents.vue')
+  },
+  {
+    path: '/events/webinar_events',
+    name: 'WebinarEvents',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "events" */ '../views/WebinarEvents.vue')
   }
 ]
 
