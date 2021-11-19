7120a1ba5642823909cf759c839918dfafeaecf3
LyallInsanity <addyvv@hotmail.nl>
2021-11-19 16:37:26 +0000

    games overview changes

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,72 +1,84 @@
 <template>
   <div class="games">
     <div class="header">
-      <h1>Hey "username", here you can find all the games we offer.<br> Click on a game to see more information.</h1>
+      <h1>
+        Hey "username", here you can find all the games we offer.<br />
+        Click on a game to see more information.
+      </h1>
     </div>
-    <div class='some-page-wrapper'>
-      <div class='row'>
-        <div class='column'>
-          <div class='column-one' onclick="location.href='https://www.247bridge.com/';">
-            <img src="../assets/bridge.png" style="height: 100px; width:180px;">Bridge
-          </div>
-          <div class='column-one' onclick="location.href='https://cardgames.io/reversi/';">
-            <img src="../assets/reversi.jpg" style="height: 100px; width:150px;">Reversi
-          </div>
-        </div>
-        <div class='column'>
-          <div class='column-two' onclick="location.href='https://cardgames.io/chess/';">
-            <img src="../assets/chess.jpg" style="height: 100px; width:150px;">Chess
-          </div>
-        </div>
-      </div>
+    <div class="games-wrapper">
+      <a onclick="location.href='https://www.247bridge.com/';">
+        <img src="../assets/bridge.png" />Bridge
+      </a>
+
+      <a onclick="location.href='https://cardgames.io/reversi/';">
+        <img src="../assets/reversi.png" />Reversi
+      </a>
+
+      <a onclick="location.href='https://cardgames.io/chess/';">
+        <img src="../assets/chess.png" />Chess
+      </a>
     </div>
   </div>
 </template>
 
 <style lang="scss">
-  .some-page-wrapper {
-    margin-top: 50px;
-  }
+.games-wrapper {
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  grid-template-columns: repeat(2, 500px);
+  column-gap: 40px;
+  row-gap: 40px;
+  margin-top: 50px;
 
-  .row {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
+  a {
+    height: 180px;
     width: 100%;
-  }
-
-  .column {
-    display: flex;
-    flex-direction: column;
-    flex-basis: 100%;
-    flex: 1;
-  }
-
-  .column-one {
-    height: 120px;
-    display: flex;
-    justify-content: space-around;
+    display: grid;
+    grid-template-columns: 150px 350px;
     align-items: center;
+    justify-items: center;
 
     border: 3px solid #d3d3d3;
     border-radius: 18px;
-    margin: 50px 50px 50px 200px;
-    font-size: 40px;
+
+    font-size: 64px;
+    font-weight: 600;
     padding-right: 100px;
     background: #f3f4f5;
-  }
 
-  .column-two {
-    height: 120px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-
-    border: 3px solid #d3d3d3;
-    border-radius: 18px;
-    margin: 50px 200px 50px 50px;
-    padding-right: 100px;
-    font-size: 40px;
-    background: #f3f4f5;
+    img {
+      width: 100px;
+      height: 80px;
+    }
   }
+}
+
+@media screen and (max-width: 1320px) {
+  .header {
+    padding: 20px 20px 0 20px;
+  }
+  .games-wrapper {
+    grid-template-columns: 500px;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  .header {
+    padding: 20px 20px 0 20px;
+  }
+  .games-wrapper {
+    grid-template-columns: repeat(1, 350px);
+    a {
+      height: 90px;
+      font-size: 32px;
+      grid-template-columns: 100px 250px;
+    }
+    img {
+      width: 70px;
+      height: 50px;
+    }
+  }
+}
 </style>
