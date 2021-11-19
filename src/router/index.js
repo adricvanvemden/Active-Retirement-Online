3b2fa5095f3a5314ddb824e992f04e299eaee6ed
LyallInsanity <addyvv@hotmail.nl>
2021-11-19 12:00:03 +0000

    fix nav

--- src/router/index.js
+++ src/router/index.js
@@ -1,102 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
-
+import SignIn from '../views/SignIn.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Dashboard',
-    component: Dashboard
+    name: 'sign_in',
+    component: SignIn
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue')
   },
   {
     path: '/events',
-    name: 'Events',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    name: 'events',
     component: () => import(/* webpackChunkName: "events" */ '../views/Events.vue')
   },
   {
+    path: '/events/food',
+    name: 'food_events',
+    component: () => import(/* webpackChunkName: "events" */ '../views/FoodEvents.vue')
+  },
+  {
+    path: '/events/games',
+    name: 'game_events',
+    component: () => import(/* webpackChunkName: "events" */ '../views/GameEvents.vue')
+  },
+  {
+    path: '/events/trips',
+    name: 'trip_events',
+    component: () => import(/* webpackChunkName: "events" */ '../views/TripEvents.vue')
+  },
+  {
+    path: '/events/webinar',
+    name: 'webinar_events',
+    component: () => import(/* webpackChunkName: "events" */ '../views/WebinarEvents.vue')
+  },
+  {
     path: '/games',
-    name: 'Games',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    name: 'games',
     component: () => import(/* webpackChunkName: "games" */ '../views/Games.vue')
   },
   {
     path: '/myAccount',
-    name: 'MyAccount',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    name: 'my_account',
     component: () => import(/* webpackChunkName: "account" */ '../views/MyAccount.vue')
   },
   {
-    path: '/signIn',
-    name: 'SignIn',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "signIn" */ '../views/SignIn.vue')
-  },
-  {
-
-    path: '/myAccount',
-    name: 'MyAccount',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/MyAccount.vue')
-  },
-  {
-    path: '/events/food_events',
-    name: 'FoodEvents',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "events" */ '../views/FoodEvents.vue')
-  },
-  {
-    path: '/events/game_events',
-    name: 'GameEvents',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "events" */ '../views/GameEvents.vue')
-  },
-  {
-    path: '/events/trip_events',
-    name: 'TripEvents',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "events" */ '../views/TripEvents.vue')
-  },
-  {
-    path: '/events/webinar_events',
-    name: 'WebinarEvents',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "events" */ '../views/WebinarEvents.vue')
-  },
-  {
     path: '/admin/events/create',
-    name: 'createEvent',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    name: 'create_event',
     component: () => import(/* webpackChunkName: "events-admin" */ '../views/CreateEvent.vue')
   },
   {
     path: '/admin/events/edit',
-    name: 'editEvent',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    name: 'edit_event',
     component: () => import(/* webpackChunkName: "events-admin" */ '../views/EditEvent.vue')
 
   }
